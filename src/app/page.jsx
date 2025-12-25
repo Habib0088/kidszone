27bcc43e45b19a9e032ad327acabda4fe0234f00
Habib0088 <sarkerbatch2k19@gmail.com>
2025-12-25 10:08:26 +0600

    Basic setup

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center  dark:bg-black">
+     <h1><button className="btn bg-secondary">Hello</button></h1>
+     
+    </div>
+  );
+}
