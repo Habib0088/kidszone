e031ad35bf31752c2e17dc30d255c538a0c776f1
Habib0088 <sarkerbatch2k19@gmail.com>
2025-12-27 22:58:50 +0600

    Login implemented

--- src/action/server/auth.js
+++ src/action/server/auth.js
@@ -1,29 +1,43 @@
-'use server'
+"use server";
 
-import { dbCollection, dbConnect } from "@/lib/dbConnect"
-import bcrypt from "bcryptjs"
-export const postUser=async(payload)=>{
-const {name,email,password}=payload
+import { dbCollection, dbConnect } from "@/lib/dbConnect";
+import bcrypt from "bcryptjs";
+export const postUser = async (payload) => {
+  const { name, email, password } = payload;
 
-// Check Payload
-if(!email|| !password) return null
-// check user
-const isExist=await dbConnect(dbCollection.USERS).findOne({email})
-if(isExist) return null
-// create user
-const newUser={
-    provider:"credentials",
+  // Check Payload
+  if (!email || !password) return null;
+  // check user
+  const isExist = await dbConnect(dbCollection.USERS).findOne({ email });
+  if (isExist) return null;
+  // create user
+  const newUser = {
+    provider: "credentials",
     name,
     email,
-    password:await bcrypt.hash(password,14),
-    role:"user",
-}
-// Insert user
-const result=await dbConnect(dbCollection.USERS).insertOne(newUser)
-if(result.acknowledged){
+    password: await bcrypt.hash(password, 14),
+    role: "user",
+  };
+  // Insert user
+  const result = await dbConnect(dbCollection.USERS).insertOne(newUser);
+  if (result.acknowledged) {
     return {
-        ...result,
-        insertedId:result.insertedId.toString()
-    }
-}
-}
+      ...result,
+      insertedId: result.insertedId.toString(),
+    };
+  }
+};
+
+export const loginUser = async (payload) => {
+  const { email, password } = payload;
+  if (!email || !password) return null;
+
+  const user = await dbConnect(dbCollection.USERS).findOne({ email });
+  if (!user) return null;
+  const passwordMatched = await bcrypt.compare(password, user.password);
+  if (passwordMatched) {
+    return user;
+  } else {
+    return null;
+  }
+};
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,68 +1,83 @@
-'use client';
-import Link from 'next/link';
-import { FcGoogle } from 'react-icons/fc';
+"use client";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import { FcGoogle } from "react-icons/fc";
+import { signIn } from "next-auth/react"
 
 const LoginForm = ({ switchForm }) => {
-  const handleSubmit = (e) => {
+  const handleSubmit =async (e) => {
     e.preventDefault();
-    console.log('Login submitted');
+
+    const form = new FormData(e.target);
+    const email = form.get("email");
+    const password = form.get("password");
+
+    
+
+    //  here call signIn / API etc.
+    await signIn("credentials", { email, password ,redirect:false})
   };
 
   const handleGoogleLogin = () => {
-    console.log('Login with Google');
+    console.log("Login with Google");
   };
 
   return (
-    
-   <div className='max-h-screen md:py-7 flex justify-center'>
-     <div className="w-full text-center  max-w-md bg-white p-8 rounded-lg shadow-2xl">
-      <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
-      <form onSubmit={handleSubmit} className="space-y-4">
-        <div>
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input
-            type="email"
-            placeholder="Your Email"
-            className="input input-bordered w-full"
-            required
-          />
-        </div>
-        <div>
-          <label className="label">
-            <span className="label-text ">Password</span>
-          </label>
-          <input
-            type="password"
-            placeholder="Password"
-            className="input input-bordered w-full"
-            required
-          />
-        </div>
-        <button type="submit" className="btn btn-primary w-full">
-          Login
-        </button>
-      </form>
+    <div className="min-h-screen md:py-7 flex justify-center">
+      <div className="w-full text-center max-w-md bg-white p-8 rounded-lg shadow-2xl">
+        <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
 
-      <button
-        onClick={handleGoogleLogin}
-        className="btn btn-outline w-full mt-4 flex items-center justify-center gap-2"
-      >
-        <FcGoogle size={24} /> Login with Google
-      </button>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              name="email"
+              type="email"
+              placeholder="Your Email"
+              className="input input-bordered w-full"
+              required
+            />
+          </div>
 
-      <p className="text-center mt-4">
-        Don't have an account?{' '}
-        <Link href={'/register'}
-          className="text-blue-500 cursor-pointer"
-          onClick={switchForm}
+          <div>
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              name="password"
+              type="password"
+              placeholder="Password"
+              className="input input-bordered w-full"
+              required
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary w-full">
+            Login
+          </button>
+        </form>
+
+        <button
+          onClick={handleGoogleLogin}
+          className="btn btn-outline w-full mt-4 flex items-center justify-center gap-2"
         >
-          Register
-        </Link>
-      </p>
+          <FcGoogle size={24} /> Login with Google
+        </button>
+
+        <p className="text-center mt-4">
+          Don't have an account?{" "}
+          <Link
+            href="/register"
+            className="text-blue-500 cursor-pointer"
+            onClick={switchForm}
+          >
+            Register
+          </Link>
+        </p>
+      </div>
     </div>
-   </div>
   );
 };
 
